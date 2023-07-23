cbb9b23c0cd7389fb4d2fab70d1585edbee3790a
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-07-23 19:19:24 +0900

    loopを実装

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,30 +1,41 @@
 import { useRouter } from "next/router";
 import styles from "../../../styles/Home.module.css";
-import YouTube, { YouTubeProps } from "react-youtube";
+import YouTube from "react-youtube";
+import { useRef } from "react";
 
 const IndividualPage = () => {
   const router = useRouter();
   const id = router.query.id as string;
   const start = Number(router.query.start);
   const end = Number(router.query.end);
+  const playerRef = useRef<any>(null);
 
-  // const onPlayerReady:YouTubeProps["onReady"]=()=>{
-
-  // }
-
-  const opts = {
-    height: "360",
-    width: "640",
-    playerVars: {
-      start: start,
-      end: end,
-    },
+  // プレーヤーの状態が変わったときに呼ばれるコールバック関数
+  const onStateChange = (event: any) => {
+    // 再生終了したら開始位置に戻す
+    if (event.data === YouTube.PlayerState.ENDED) {
+      playerRef.current?.internalPlayer?.seekTo(start);
+      playerRef.current?.internalPlayer?.playVideo();
+    }
   };
 
   return (
     <>
       <main className={styles.main}>
-        <YouTube videoId={id} opts={opts} />
+        <YouTube
+          videoId={id}
+          opts={{
+            height: "360",
+            width: "640",
+            playerVars: {
+              start: start,
+              end: end,
+              loop: 1,
+            },
+          }}
+          onStateChange={onStateChange}
+          ref={playerRef}
+        />
         <h2>{router.query.title}</h2>
         <p>{router.query.id}</p>
         <p>{router.query.start}</p>
